da606ef13e825f91438dc13e6c594989b40e8b23
Chris <christopherhung96@gmail.com>
2017-03-07 20:39:35 -0500

    added a queue

--- app/views.py
+++ app/views.py
@@ -1,25 +1,35 @@
-from flask import render_template, request
-from app import app
+from flask import render_template, request, redirect, url_for
+from app import app, queue
+from time import sleep
 
 @app.route('/', methods = ['POST','GET'])
+
+
 def index():
+	global queue
 	if(request.method == 'POST'):
 		if (request.form['submit'] == 'Up'):
 			#call func to make robot go up
 			print("Moving robot up")
+			queue.append("Up")
 		elif (request.form['submit'] == 'Down'):
 			#call func to make robot go down
 			print("Moving robot down")
+			queue.append("Down")
 		elif (request.form['submit'] == 'Left'):
 			#call func to make robot go left
 			print("Moving robot left")
+			queue.append("Left")
 		elif (request.form['submit'] == 'Right'):
 			#call func to make robot go right
 			print("Moving robot right")
+			queue.append("Right")
 		elif (request.form['submit'] == 'Rotate CW'):
 			#call func to rotate robot CW
 			print("Rotating robot clockwise")
+			queue.append("CW")
 		elif (request.form['submit'] == 'Rotate CCW'):
 			#call func to rotate robot CCW
 			print("Rotating robot counterclockwise")
-	return render_template('untitled.html',title = 'Pi Controller',name = 'World')
+			queue.append("CCW")
+	return render_template('untitled.html',title = 'Pi Controller',name = 'World', actionQueue = str(queue))
